c1c233247d512c68ad6ed19647772136fed259c8
Ethan Brady <50305557+untitledmind@users.noreply.github.com>
2019-12-12 23:40:14 -0500

    scrape-wapo
    change string concatenation method

--- scrape-wapo.py
+++ scrape-wapo.py
@@ -1,6 +1,7 @@
 """Gets the top story on the Washington Post at runtime,
 then writes a formatted txt file of the article's content
-and saves the main image as jpg"""
+and saves the main image as jpg
+"""
 
 import requests
 import json
@@ -13,8 +14,10 @@ def parse_date(arg):
     """Takes publish_date string from WaPo soup
     and returns the date as datetime object
     Time string from soup sometimes includes
-    milliseconds, sometimes not"""
+    milliseconds, sometimes not
+    """
     import datetime
+    import sys
     for fmt in ('%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%dT%H:%M:%S.%fZ'):
         try:
             dt = datetime.datetime.strptime(arg, fmt)
@@ -27,24 +30,25 @@ def parse_authors(arg):
     """Parses soup for author names
     Author names within json sometimes
     appear in list of dicts, sometimes
-    in dict"""
+    in dict
+    """
     if isinstance(arg, list):
-        names = ''
-        for item in arg:
-            names += item['name'] + ', '
-        names = names[:-2]
+        names = ', '.join(arg['name'])[:-2]
         return names
     elif isinstance(arg, dict):
         names = arg['name']
         return names
 
-def soupify():
+def soupify(url):
     """pulls soup from site and processes into text"""
-    main_url = 'https://www.washingtonpost.com'
-    response1 = requests.get(main_url)
+    response1 = requests.get(url)
     main_soup = BeautifulSoup(response1.text, 'html.parser')
 
-    top_story_url = main_soup.find('h1').find('a')['href']
+    try:
+        top_story_url = main_soup.find('h1').find('a')['href']
+    except AttributeError:
+        top_story_url = main_soup.find('div', {'class': 'headline normal x normal-style'}).find('a')['href']
+
     response2 = requests.get(top_story_url)
     story_soup = BeautifulSoup(response2.text, 'html.parser')
 
@@ -62,14 +66,13 @@ def soupify():
 
     return story_soup, title, subtitle, authors, publish_date, modify_date, image_url
 
-def format_elements():
+def format_elements(story_soup, title, subtitle, authors, publish_date, modify_date, image_url):
     """takes soup objects, turns into formatted text and stores in lists"""
-    story_soup, title, subtitle, authors, publish_date, modify_date, image_url = soupify()
     elements = [title.upper(),
-                '**' + subtitle + '**',
-                'By ' + parse_authors(authors),
+                f'**{subtitle}**',
+                f'By {parse_authors(authors)}',
                 parse_date(publish_date).strftime('%b. %d, %Y'),
-                'Updated ' + parse_date(modify_date).strftime('%-H:%M')
+                f"Updated {parse_date(modify_date).strftime('%-H:%M')}"
                 ]
 
     grafs = story_soup.find_all('p')
@@ -88,14 +91,14 @@ def format_elements():
 
 def write_story():
     """takes soup and writes to file"""
-    elements = format_elements()
+    url = 'http://www.washingtonpost.com'
+    elements = format_elements(*soupify(url))
     filename = 'wapo_top_story.txt'
     with open(filename, 'w') as story:
         for e in elements:
             story.write(e)
             story.write('\n\n')
         story.write('========')
-    story.close()
 
 if __name__ == '__main__':
     write_story()
